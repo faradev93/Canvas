8915a74328c878a75792b5426451ef2b2bddc607
faradev93 <faramarz.rezakhanlou.sf@gmail.com>
2025-03-30 13:11:33 +0330

    test

--- script2.js
+++ script2.js
@@ -2,33 +2,35 @@
 //   let feriCanvas = document.getElementById("feriCanvas");
 //   let context = feriCanvas.getcotext("2d");
 // };
-let canvas = document.getElementById("feriCanvas");
-const ctx = canvas.getContext("2d");
+window.onload = function (e) {
+  let canvas = document.getElementById("feriCanvas");
+  const ctx = canvas.getContext("2d");
 
-let radius1 = 50;
-let radius2 = 30;
-const centerX = canvas.width / 2;
-const centerY = canvas.height / 2;
+  let radius1 = 50;
+  let radius2 = 30;
+  const centerX = canvas.width / 2;
+  const centerY = canvas.height / 2;
 
-function drawCircles() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  function drawCircles() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  ctx.beginPath();
-  ctx.arc(centerX, centerY, radius1, 0, 2 * Math.PI);
-  ctx.fillStyle = "blue";
-  ctx.fill();
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, radius1, 0, 2 * Math.PI);
+    ctx.fillStyle = "blue";
+    ctx.fill();
 
-  ctx.beginPath();
-  ctx.arc(centerX, centerY, radius2, 0, 2 * Math.PI);
-  ctx.fillStyle = "red";
-  ctx.fill();
-}
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, radius2, 0, 2 * Math.PI);
+    ctx.fillStyle = "red";
+    ctx.fill();
+  }
 
-function increaseSize() {
-  radius1 += 1;
-  radius2 += 1;
+  function increaseSize() {
+    radius1 += 1;
+    radius2 += 1;
 
-  drawCircles();
-}
+    drawCircles();
+  }
 
-setInterval(increaseSize, 10);
+  setInterval(increaseSize, 10);
+};
